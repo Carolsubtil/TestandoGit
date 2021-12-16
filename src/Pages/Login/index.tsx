e78c6cdf82419057ce71df9b0de76551c89a7b16
Carolsubtil <carolgosliski@gmail.com>
2021-12-15 20:53:31 -0400

    Styles em arquivos diferentes

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
-import { StyleSheet } from 'react-native';
+import {styles} from './styles';
 
 const Login = () => {
     return(
@@ -21,48 +21,5 @@ const Login = () => {
     );
 }
 
-const styles = StyleSheet.create({
-    body: {
-        flex:1,
-        backgroundColor: "#4e71ba",
-        justifyContent: "center",
-    },
-    img:{
-        width: 50,
-        height: 50,
-        backgroundColor: "white",
-        marginRight: 25,
-        marginTop: 10,
-    },
-    face:{
-        fontSize: 50,
-        color: "white",
-        fontWeight: "bold",
-        paddingTop: -30,
-    },
-    input:{
-        width: 300,
-        backgroundColor: "white",
-        height: 50,
-        marginTop: 20,
-        color: "gray",
-        paddingTop: 15,
-        paddingLeft: 15,
-        fontSize: 15,
-        
-    },
-    login:{
-        width: 300,
-        backgroundColor: "#2f477a",
-        height: 50,
-        marginTop: 20,
-        color: "white",
-        paddingTop: 10,
-        paddingLeft: 119,
-        fontSize: 20,
-        fontWeight: "bold",
-        
-    },
-});
 
 export default Login;
