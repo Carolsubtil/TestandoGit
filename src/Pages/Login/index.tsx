717d98e255b5c9ce26857943e4331f2d0e7c2a25
Carolsubtil <carolgosliski@gmail.com>
2021-12-15 21:07:59 -0400

    Tarefa Commit botao de Log In

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import {styles} from './styles';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import { StackNavigatorParamList } from '../../../types';
+
+type LoginProps = NativeStackNavigationProp<StackNavigatorParamList,"Login">;
 
 const Login = () => {
+
+    const navigation = useNavigation<LoginProps>();
+
+    function irParaTelaHome(){
+        navigation.navigate('Home');
+    }
     return(
         <View style ={styles.body}>
             <View style={{justifyContent: "center", marginLeft: 50,}}>
@@ -12,7 +23,9 @@ const Login = () => {
                 <View style={{marginTop: 40}}>
                     <Text style={styles.input}>Email</Text>
                     <Text style={styles.input}>Password</Text>
+                    <TouchableOpacity onPress={irParaTelaHome}>
                     <Text style={styles.login}>Log In</Text>
+                    </TouchableOpacity>
                 </View>
                 <Text style={{color: "white",marginLeft: 85, marginTop: 100,}}>Sing Up for Facebook</Text>
             </View>
