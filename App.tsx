d327e767bbaa37a812cf5e59841f3c8e77265c78
Carolsubtil <carolgosliski@gmail.com>
2021-12-15 20:43:40 -0400

    Tarefa Commit da Tela de Login

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {Text, View, StyleSheet } from 'react-native';
-import  Routes from './src/routes'
+import  Routes from './src/Routes'
 
 
 export default function App() {
--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,12 +1,68 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 const Login = () => {
     return(
-        <View>
-            <Text>Login</Text>
+        <View style ={styles.body}>
+            <View style={{justifyContent: "center", marginLeft: 50,}}>
+                <View style={{flexDirection:"row"}}>
+                    <Image source={require("../../assets/facebook.png")} style= {styles.img}></Image><Text style= {styles.face}>Facebook</Text>
+                </View>
+                <View style={{marginTop: 40}}>
+                    <Text style={styles.input}>Email</Text>
+                    <Text style={styles.input}>Password</Text>
+                    <Text style={styles.login}>Log In</Text>
+                </View>
+                <Text style={{color: "white",marginLeft: 85, marginTop: 100,}}>Sing Up for Facebook</Text>
+            </View>
+            
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    body: {
+        flex:1,
+        backgroundColor: "#4e71ba",
+        justifyContent: "center",
+    },
+    img:{
+        width: 50,
+        height: 50,
+        backgroundColor: "white",
+        marginRight: 25,
+        marginTop: 10,
+    },
+    face:{
+        fontSize: 50,
+        color: "white",
+        fontWeight: "bold",
+        paddingTop: -30,
+    },
+    input:{
+        width: 300,
+        backgroundColor: "white",
+        height: 50,
+        marginTop: 20,
+        color: "gray",
+        paddingTop: 15,
+        paddingLeft: 15,
+        fontSize: 15,
+        
+    },
+    login:{
+        width: 300,
+        backgroundColor: "#2f477a",
+        height: 50,
+        marginTop: 20,
+        color: "white",
+        paddingTop: 10,
+        paddingLeft: 119,
+        fontSize: 20,
+        fontWeight: "bold",
+        
+    },
+});
 
 export default Login;
